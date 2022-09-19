9a4d76b2b10e682de70fe950b6055b6523fbdb15
Leonardo Fabian <ramonlfabian@gmail.com>
2022-09-18 20:42:45 -0400

    Create Category form

---
+++ CarRental/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create( Categories category)
+        {
+            return View();
+        }
+    }
+}
---
+++ CarRental/Models/Categories.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRental.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CostPerDay { get; set; }
+        public int Passengers { get; set; }
+        public int Luggages { get; set; }
+        public decimal LateFeePerHour { get; set; }
+    }
+}
